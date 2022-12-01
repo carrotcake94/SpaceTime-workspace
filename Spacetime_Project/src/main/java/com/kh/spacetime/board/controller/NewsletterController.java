443ded36c4906f7371d5056f5cd4a197f4d0f53b
heesupsin <ksronh@naver.com>
2022-12-01 16:37:06 +0900

    test를 위한 주석-신희섭
    NewsletterController.java의
    10라인 주석 신희섭

--- Spacetime_Project/src/main/java/com/kh/spacetime/board/controller/NewsletterController.java
+++ Spacetime_Project/src/main/java/com/kh/spacetime/board/controller/NewsletterController.java
@@ -7,7 +7,7 @@ import com.kh.spacetime.board.model.service.NewsletterService;
 
 @Controller
 public class NewsletterController {
-	//20221201 테스트 -혜민 
+	//20221201 테스트 -신희섭 
 	@Autowired
 	private NewsletterService newsletterService;
 }
